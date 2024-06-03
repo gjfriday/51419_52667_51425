3813609226d349f5683dc21eae8efd0fb5e661e9
gjfriday <gpiatkowski100@gmail.com>
2024-06-03 19:28:24 +0200

    Create index.php
    Szkielet formularza do logowania do aplikacji.

---
+++ projekt/ankieta/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pl">
+  <head>
+    <title>Ankieter - proste ankiety</title>
+  </head>
+  <body>
+        <div>Logowanie</div>
+        <form>
+          <div>
+              <input placeholder="Login">
+          </div><br>
+          <div>
+            <input placeholder="Hasło">
+          </div><br>
+
+          <button>Zaloguj</button>
+        </form><hr>
+        <div><a href="">Utwórz konto</a></div>
+  </body>
+</html>
