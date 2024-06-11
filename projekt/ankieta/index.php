37b7ebded8a81244d84aae1d5d32b8a0b9b8c0e2
gjfriday <gpiatkowski100@gmail.com>
2024-06-11 13:58:10 +0200

    Update index.php
    Dodanie weryfikacji, czy użytkownik jest już zalogowany w celu uniknięcia nieplanowanego wylogowania.

--- projekt/ankieta/index.php
+++ projekt/ankieta/index.php
@@ -1,8 +1,19 @@
 <?php
   session_start();
+
+//Weryfikacja, czy użytkownik jest już zalogowany i przekierowuje z powrotem do odpowiedniej strony.
+  if (isset($_SESSION["IS_VALID_LOGIN"]) && $_SESSION["IS_VALID_LOGIN"] == 1) {
+    if (isset($_SESSION["ADMIN_LOGGED_IN"]) && $_SESSION["ADMIN_LOGGED_IN"] == 1) {
+        header("Location: admin_homepage.php");
+        exit();
+    } elseif (isset($_SESSION["USER_LOGGED_IN"]) && $_SESSION["USER_LOGGED_IN"] == 1) {
+        header("Location: user_homepage.php");
+        exit();
+    }
+}
+
   include("view/header.php");
 
-  $_SESSION['start']=1;
 ?>
 
   <body>
@@ -14,15 +25,16 @@
     <div class=container>
       <div id ="login">
         <div id="loginTitle">Logowanie</div>
-          <form id = "loginForm" class="form-inline" method="post" action="login_authenticate.php">
+          <form id = "loginForm" class="form-inline" method="post" action="login_auth.php">
             <div class="form-group row">
                 <input type="email" name="email" class="form-control" id="email" placeholder="Login">
             </div><br>
             <div class="form-group row">
               <input type="password"  name="password" class="form-control" id="password" placeholder="Hasło">
             </div><br>
-
+        
         <?php 
+
         {
           if(isset($_SESSION['IS_VALID_LOGIN']) && $_SESSION['IS_VALID_LOGIN']==0) 
           echo "Błędny login / hasło!";
@@ -36,5 +48,7 @@
       </div>
     </div>
 
+  </body>
+</html>
   </body>
 </html>
