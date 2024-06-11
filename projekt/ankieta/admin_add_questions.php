d0615e1827c8a6baa99e1a87ea1b4f4bf4b145aa
gjfriday <gpiatkowski100@gmail.com>
2024-06-11 14:48:00 +0200

    Update admin_add_questions.php
    Wyniesienie części kodu do innych plików.

--- projekt/ankieta/admin_add_questions.php
+++ projekt/ankieta/admin_add_questions.php
@@ -1,10 +1,8 @@
 <?php
-//include('/ankieta/config.php');
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "ankieta";
+include("config/config.php");
+include("view/header.php");
+include("view/admin_view.php");
 
 $question_contentErr = "";
 $question_content = ""; 
@@ -21,25 +19,41 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
 
-<!DOCTYPE html>
-<html>
-<!--Tu będzie layout-->
+	<form id="addQuestions" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 
-	
+	  Treść pytania:
+	  <input type="text" name="question_content" value="<?php echo $question_content;?>">
+	  	<?php if ($_SERVER["REQUEST_METHOD"] == "POST") { if ($question_content == "") { ?>
+		<span class="error">* <?php echo $question_contentErr;?></span>
+	<?php } }?>
+	<br><br>
+
+	  Tytuł pytania:
+	  <input type="text" name="question_name" value="<?php echo $question_name;?>">
+		<br><br>
+
+	  <input type="submit" value="Dodaj">
+	  <br><br>
+	</form>
+
 <?php
+
 if ($question_content!= "") {
 
-  $con = mysqli_connect($servername, $username, $password, $dbname);
+    $con = mysqli_connect($servername, $username, $password, $dbname);
     if(!$con) {
         die("Nie nawiązano połączenia z bazą danych: " . mysqli_connect_error());
     }
 
+	// $question_id=0;
+	// $num_rows=0;
+
 	$sql = "
-  	INSERT INTO tbl_questions(question_name, question_content) 
-  	VALUES ('$question_name', '$question_content')
+	INSERT INTO tbl_questions(question_name, question_content) 
+	VALUES ('$question_name', '$question_content')
 	";
-  
-  if (mysqli_query($con, $sql)) {
+
+	if (mysqli_query($con, $sql)) {
 		$question_id = mysqli_insert_id($con);
 		echo "Nowe pytanie utworzone. question_id: " . $question_id;
 	} else
@@ -50,5 +64,9 @@ if ($question_content!= "") {
 	mysqli_close($con);
 }
 ?>
+
+</body>
+</html>
+
 
 </html>
