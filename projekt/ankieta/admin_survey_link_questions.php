254da04a3256cd15bc1dcb8ec95d7977e96d3c76
gjfriday <gpiatkowski100@gmail.com>
2024-06-11 15:14:03 +0200

    Update admin_survey_link_questions.php
    Uzupełnienie o widok i wyniesienie części kodu do osobnych plików

--- projekt/ankieta/admin_survey_link_questions.php
+++ projekt/ankieta/admin_survey_link_questions.php
@@ -1,15 +1,12 @@
 <?php
-//include('/ankieta/config.php');
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "ankieta";
+include("config/config.php");
+include("view/header.php");
+include("view/admin_view.php");
 
 ?>
 
-<html>
-
+<div id="contentLinkQ">
   
 <?php
 $con = mysqli_connect($servername, $username, $password, $dbname);
@@ -65,4 +62,68 @@ echo "<br> Liczba pytań: ".$questions_count."<br>";
 #$con->close();
 ?>
 
+<br><br>
+<form  method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+  
+  <select name="survey_id_list">
+<?php for ($i=1; $i<=$survey_count; $i++)
+	{?>
+    <option value="<?php echo "$survey_id_array[$i]";?>"><?php echo "$survey_names_array[$i]";?></option>
+    <?php
+	}?>
+  </select>
+
+   <select name="question_id_list">
+<?php for ($i=1; $i<=$questions_count; $i++)
+	{?>
+    <option value="<?php echo "$questions_id_array[$i]";?>"><?php echo "$questions_content_array[$i]";?></option>
+    <?php
+	}?>
+  </select>
+
+  <br><br>
+  <input type="submit"  value="Połącz">
+</form>
+
+<?php
+
+$question_id = $survey_id = -1;
+if ($_SERVER["REQUEST_METHOD"] == "POST")
+{
+	$question_id=$_POST["question_id_list"];
+	$survey_id=$_POST["survey_id_list"];
+	
+
+	$sql2="SELECT * FROM tbl_survey_questions WHERE survey_id=$survey_id AND question_id=$question_id";
+    $result2 = mysqli_query($con, $sql2);
+	$num_rows = mysqli_num_rows($result2);
+	
+
+    if($num_rows == 0) 
+    {
+		$sql = "
+      INSERT INTO tbl_survey_questions 
+      VALUES (null, '$survey_id', '$question_id')
+      ";
+
+		
+		if (mysqli_query($con, $sql))
+		{
+			$survey_question_id = mysqli_insert_id($con);
+			echo "Pytanie nr: ".$question_id . " zostało powiązane z ankietą nr: " . $survey_id;
+		} else
+		{
+			echo "Błąd " . $sql . "<br>" . mysqli_error($con);
+		}
+    } else
+	{
+		echo "Pytanie nr: ".$question_id . " jest już powiązane z ankietą nr: " . $survey_id;
+	}
+}
+
+$con->close();
+
+?>
+</div>
+</body>
 </html>
