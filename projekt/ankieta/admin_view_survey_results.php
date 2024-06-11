e9a08c3fbf9871e193ed52fd9df0840f774abcdc
gjfriday <gpiatkowski100@gmail.com>
2024-06-11 15:51:14 +0200

    Update admin_view_survey_results.php
    Przeniesienie części kodu do osobnych plików;

--- projekt/ankieta/admin_view_survey_results.php
+++ projekt/ankieta/admin_view_survey_results.php
@@ -1,14 +1,13 @@
 <?php
 
-// include('/ankieta/config.php');
-
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "ankieta";
+include("config/config.php");
+include("view/header.php");
+include("view/admin_view.php");
 
 ?>
-<html>
+
+
+<div id="surveyList">
 
 <?php
 
@@ -25,7 +24,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 	$survey_id=$_POST["survey_id_list"];
 	echo "<br>Numer ankiety: ".$survey_id."<br>";
 
-
 	$sql="SELECT tbl_survey_questions.question_id,
        tbl_questions.question_content,
        tbl_survey_questions.survey_id
@@ -36,23 +34,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 
     $result = mysqli_query($con, $sql);
 	$num_rows = mysqli_num_rows($result);
+	
 
-    if($num_rows == 0) 
+    if($num_rows == 0) // Wyjście w przypadku braku pytań powiązanych z daną ankietą.
     {
-		echo "Brak pytań powiązanych z tą ankietą.";
+		echo "Brak pytań powiązanych z tą ankietą!";
 		$con->close();
 		exit;
 	}
 
 	for ($x=1; $x<=$num_rows; $x++)
 	{
-		for ($y=1; $y<=5; $y++) 
+		for ($y=1; $y<=5; $y++) // Change to options count
 		{
 			$response_count[$x][$y]=0;
 		}
 	}
 
-  	$questions_count=0;
+	$questions_count=0;
 
 	while($row=mysqli_fetch_array($result))
     {
@@ -61,7 +60,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 		$questions_id_array[$questions_count] = $row['question_id'];
 		$question_id=$questions_id_array[$questions_count];
 
-  		for ($i=1; $i<=5; $i++)  
+		// Licznik odpowiedz dla każdej odpowiedzi.
+		for ($i=1; $i<=5; $i++)  // Pięć odpowiedzi.
 		{
 				$response=$i;
 				$sql1="SELECT
@@ -85,9 +85,38 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 				$num_rows = mysqli_num_rows($result1);
 				if($row1=mysqli_fetch_array($result1,MYSQLI_ASSOC))
 				{
-				
-						$response_count[$questions_count][$response]=$row1['COUNT(tbl_user_questions.response)'];
-
+					$response_count[$questions_count][$response]=$row1['COUNT(tbl_user_questions.response)'];
+						
 				}
 		}
     }
+
+	echo "<table width='386' border='1'> <tr><th>S.No.</th><th>Question Id</th><th>Question</th>
+	<th>Response-1</th>
+	<th>Response-2</th>
+	<th>Response-3</th>
+	<th>Response-4</th>
+	<th>Response-5</th>
+	</tr>";
+	echo "<caption> <b> Responses to questions </b> </caption>";
+	$i=0;
+	while($i<$questions_count)
+	    {
+		$i++;
+		echo "<tr><td>".$i."</td><td>".$questions_id_array[$i]."</td><td>".$questions_content_array[$i]."</td>";
+		for ($j=1; $j<=5; $j++)
+		{
+			echo "<td>".$response_count[$i][$j]."</td>";
+		}
+		echo "</tr>";
+	    }
+
+
+}
+
+$con->close();
+
+?>
+</div>
+</body>
+</html>
