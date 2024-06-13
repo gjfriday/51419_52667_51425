03ac3b224d81fce26e65de0c322878384194c829
gjfriday <gpiatkowski100@gmail.com>
2024-06-13 13:49:50 +0200

    Create user_view.php
    Widok użytkownika

---
+++ projekt/ankieta/view/user_view.php
@@ -0,0 +1,12 @@
+<body>
+<div id="nav">
+    <ul class="pull-left">
+      <li><a href="user_homepage.php">Panel Użytkownika</a></li>
+      <li>/</li>
+      <li><a href="user_view_survey_list.php">Wypełnij ankietę</a></li>
+    </ul>
+    <div id="logo"><a href="#"><img src="img/logoankieter.png" height="35" width="62.2"></a></div>
+    <ul class="pull-right">
+      <li><a href="logout.php">WYLOGUJ</a></li>
+    </ul>
+</div>
