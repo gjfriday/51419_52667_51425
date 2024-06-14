c78fdab35ae392b92c77479229dfefbda4547883
gjfriday <gpiatkowski100@gmail.com>
2024-06-14 12:12:57 +0200

    Testowanie: admin_view_survey_list.php
    Poprawa błędów

--- projekt/ankieta/admin_view_survey_list.php
+++ projekt/ankieta/admin_view_survey_list.php
@@ -19,7 +19,7 @@ if(!$con)
 mysqli_select_db($con, $dbname);
 
 // Pobierz listę ankiet z bazy danych
-echo "<table width='1000' border='1'> <tr><th>Numer ankiety</th><th>Tytuł</th><th>Opis</th><th>Data rozpoczęcia</th><th>Data zakończenia</th></tr>";
+echo "<table width='1000' border='1'> <tr><th>Numer ankiety</th><th>Tytuł</th><th>Opis</th>";
 echo "<caption> <b> Lista ankiet </b> </caption>";
 $sql="SELECT * FROM tbl_surveys";
 $result = mysqli_query($con, $sql);
@@ -30,7 +30,7 @@ while($row=mysqli_fetch_array($result))
 	$survey_count++;
 	$survey_names_array[$survey_count] = $row['survey_name'];
 	$survey_id_array[$survey_count] = $row[0];
-	echo "<tr><td>".$row[0]."</td><td>".$row['survey_name']."</td><td>".$row['survey_description']."</td><td>".$row['survey_open_date']."</td><td>".$row['survey_close_date']."</td></tr>";
+	echo "<tr><td>".$row[0]."</td><td>".$row['survey_name']."</td><td>".$row['survey_description']."</td></tr>";
 }
 echo "</table>";
 echo "<br> Liczba ankiet: ".$survey_count."<br>";
